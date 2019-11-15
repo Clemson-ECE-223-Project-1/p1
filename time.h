a0189df21bf9b43f316a336ca0fa5d0a319ff6a9
epriole <57415394+epriole@users.noreply.github.com>
2019-11-14 19:57:09 -0500

    Update time.h

--- time.h
+++ time.h
@@ -1,16 +1,30 @@
 /* time.h */
 
+double sim_time;
+
 /* creates a double for using as the global sim time */
-void time_init();
+void time_init()
+{
+    sim_time = 500;
+}
 
 /* cleans up the time system as needed */
-void time_fini();
+void time_fini()
+{
+
+}
 
 /* returns the current sim time */
-double time_get();
+double time_get()
+{
+    return sim_time;
+}
 
 /* sets the current sim time */
-void time_set(double t);
+void time_set(double t)
+{
+    sim_time = t;
+}
 
 /* these return a random time which is used to set the
    time each passenger takes to make each of these steps
