2e6986650e5c28211e3744b5a47c72c35b140401
briorda <35381658+briorda@users.noreply.github.com>
2019-11-14 19:55:01 -0500

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,33 +1,63 @@
-#include <stdlib.h>
-#include <stdio.h>
+/* priority.c */
+
 #include "priority.h"
 #include "event.h"
+#include "queue.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
-//initialize the priority heap-array
-priority_t *priority_init(int size)
-{
-    if (size < 0)
+priority_t *priority_init(int size) {
+    if(size < 0) {
+        printf("Size < 0\n");
         return NULL;
+    }
+    
+    priority_t *q = (priority_t *)malloc(sizeof(priority_t));
+    q->max_size = size; // maximum size of tree
+    q->current_size = 0;   // number of nodes in tree
+    q->event_tree = (event_t **)malloc(sizeof(event_t)*(size+1)); // tree
+    
+    for(int i = 0; i < size; i++) {
+        q->event_tree[i] = NULL;
+    }
 
-     priority_t *p = (priority_t *)malloc(sizeof(priority_t));
-     p->max_size = size;
-     p->current_size = 0;
-     p->event_tree = (event_t *)malloc(size*sizeof(event_t));
-
-     for (int i = 0; i < size; i++)
-        p->event_tree[i] = NULL;
-
-    if(p == NULL)
+    if(q == NULL) {
+        printf("Cannot Intialize Priority Queue\n");
         return NULL;
+    }
 
-    return p;
+    return q;
+}
+
+int priority_empty(priority_t *q) {
+    int isEmpty = -1;
+
+    if(q->current_size == 0) {
+        isEmpty = 1;
+        printf("Queue Empty\n");
+    } else {
+        isEmpty = 0;
+    }
+
+    return isEmpty;
+}
+
+int priority_full(priority_t *q) {
+    int isFull = 0;
+
+    if(q->current_size == q->max_size) {
+        isFull = 1;
+        printf("Queue Full\n");
+    }
+
+    return isFull;
 }
 
 //Function to make the array into a heap
 static void heapify(priority_t *q, event_t *ev)
 {//increasing the size of the array by 1 since the first element in the array needs to be empty
-    q->current_size = q->current_size++;
+    q->current_size++;
     int child_pos, parent_pos;
     child_pos = q->current_size;
     parent_pos = child_pos/2;
@@ -52,32 +82,48 @@ static void heapify(priority_t *q, event_t *ev)
     q->event_tree[child_pos] = ev;
 }
 
-static void reheapify(priority_t *q)
-{
+int priority_insert(priority_t *q, event_t *ev) {
+    int full, empty, success;
+
+    full = priority_full(q);
+    empty = priority_empty(q);
+    success = 1;
+
+    if(full == 1) {
+        success = -1;
+    } else if(empty == 1) {
+        q->current_size++;
+        q->event_tree[1] = ev;
+        success = 0;
+    } else {
+        heapify(q, ev);
+        success = 0;
+    }
+
+    return success;
+}
+
+static void reheapify(priority_t *q) {
     int current_pos = 1;
     int child_pos = 2*current_pos;
-    event_t ev;
-    ev = q->event_tree[eq->tree_size];
-    q->event_tree[q->tree_size] = NULL;
-    q->tree_size--;
+    event_t *ev;
+    ev = q->event_tree[q->current_size];
+    q->event_tree[q->current_size] = NULL;
+    q->current_size--;
 
-    if(priority_empty(q) != 1)
-    {
-        while(child_pos <= q->tree_size)
-        {
-            if(child_pos < q->tree_size)
-            {
+    if(priority_empty(q) != 1) {
+        while(child_pos <= q->current_size) {
+            if(child_pos < q->current_size) {
                 double right_child = q->event_tree[child_pos+1]->event_time;
                 double left_child = q->event_tree[child_pos]->event_time;
 
                 if(right_child < left_child)
-                    child_pos = child_pos++;
+                    child_pos++;
             }
 
             if(q->event_tree[child_pos]->event_time >= ev->event_time)
                 break;
-            else
-            {
+            else {
                 q->event_tree[current_pos] = q->event_tree[child_pos];
                 current_pos = child_pos;
                 child_pos = 2*child_pos;
@@ -88,84 +134,38 @@ static void reheapify(priority_t *q)
     }
 }
 
-//insert an event into the priority heap-array.
-//0 for success, -1 otherwise
-int priority_insert(priority_t *q, event_t *ev)
-{
-    int full;
-    int empty;
-    int success;
-
-    full = priority_full(q);
-    empty = priority_empty(q);
-
-    if(full == 1)
-        success = -1;
-    else if(empty == 1)
-    {
-        q->current_size = q->current_size++;
-        q->event_tree[1] = ev;
-        success = 0;
-    }
-    else
-    {
-        heapify(q, ev);
-        success = 0;
-    }
-    
-}
-
 //remove the root (highest priority) of the heap
 event_t *priority_remove(priority_t *q)
 {
-    int empty;
-    empty = priority_empty(q);
-    event_t removal;
+    int empty = priority_empty(q);
+    event_t *removal;
     
-    if(empty == 1)
+    if(empty == 1) {
         removal = NULL;
-    else
-    {
+    } else {
         removal = q->event_tree[1];
+        q->event_tree[1] = q->event_tree[q->current_size];
         reheapify(q);
     }
 
     return removal;
 }
 
-//check to see if the heap is empty
-//1 if empty, 0 otherwise
-int priority_empty(priority_t *q)
-{
-    int empty;
-
-    if (q->count == 0)
-        empty = 1;
-    else
-        empty = 0;
-
-    return empty;
-}
-
-//check to see if the heap is full
-//1 if full, 0 otherwise
-int priority_full(priority_t *q)
-{
-    int full;
-
-    if(q->count == q->size)
-        full = 1;
-    else
-        full = 0;
+void print_heap(priority_t *q) {
+    int isEmpty = priority_empty(q);
     
-    return full;
+    if(isEmpty) {
+        return;
+    } else {
+        for(int i = 1; i <= q->current_size; i++) {
+            printf("%f\n", q->event_tree[i]->event_time);
+        }
+        printf("\n");
+    }
 }
 
-//free pointers used by the heap, and then free the heap
-void priority_finalize(priority_t *q)
-{
-    for(int i = 0; i < q->current_size; i++)
-    {
+void priority_finalize(priority_t *q) {
+    for(int i = 0; i < q->current_size; i++) {
         free(q->event_tree[i]);
         q->event_tree[i] = NULL;
     }
