d63ea1aac5d387021f140496c551a7e7c030eb95
briorda <35381658+briorda@users.noreply.github.com>
2019-11-14 22:03:32 -0500

    Update time.h

--- time.h
+++ time.h
@@ -1,67 +1,24 @@
 /* time.h */
 
-double sim_time;
-
 /* creates a double for using as the global sim time */
-void time_init()
-{
-    sim_time = 500;
-}
+void time_init();
 
 /* cleans up the time system as needed */
-void time_fini()
-{
-
-}
+void time_fini();
 
 /* returns the current sim time */
-double time_get()
-{
-    return sim_time;
-}
+double time_get();
 
 /* sets the current sim time */
-void time_set(double t)
-{
-    sim_time = t;
-}
+void time_set(double t);
 
 /* these return a random time which is used to set the
    time each passenger takes to make each of these steps
    these times are set into the corresponding event 
    before it is scheduled. */
-double time_arrive() {
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
-
-double time_airline() {
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
-
-double time_id() {
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
-
-double time_scan() { 
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
-
-double time_train() {
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
-
-double time_gate() {
-    srand(time(NULL));
-    double randnum = rand();
-    return randnum;
-}
+double time_arrive();
+double time_airline();
+double time_id();
+double time_scan();
+double time_train();
+double time_gate();
