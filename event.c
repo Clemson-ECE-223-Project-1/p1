4887298440a66f6fbd928046af6d1b83bf65d7b3
briorda <35381658+briorda@users.noreply.github.com>
2019-11-16 17:00:06 -0500

    Update event.c

--- event.c
+++ event.c
@@ -2,6 +2,10 @@
 
 #include "priority.h"
 #include "time.h"
+#include "randsim.h"
+#include "sim.h"
+#include "event.h"
+#include "queue.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,15 +14,12 @@ static priority_t *q = NULL;
 
 /* initializes events, creates a priority queue */
 void event_init() {
-    if(q == NULL)
-    {
-        q = priority_init(3);
-        if(q == NULL)
-        {
+    if(q == NULL) {
+        q = priority_init(MAX_PASS);
+        if(q == NULL) {
             printf("Cannot Intialize Event\n");
         }
     }
-
 }
 
 /* frees up all event space, including space in the priority queue */
@@ -42,6 +43,7 @@ event_t *event_create() {
 /* free an event */
 void event_destroy(event_t *e) {
     free(e->passenger);
+    free(e->queue);
     free(e);
 }
 
@@ -50,21 +52,41 @@ void event_destroy(event_t *e) {
    event.  Update the event time to the key value. */
 void event_schedule(event_t *e) {
     double key_time = time_get();
-    key_time += e->event_time;
+    e->event_time += key_time; 
     priority_insert(q, e);
 }
 
 /* remove the next event from the priority and return it to the program for execution */
 event_t *event_cause() {
     event_t *event = priority_remove(q);
-    if(event == NULL)
-    {
+    if(event == NULL) {
         printf("The queue is empty.");
         return NULL;
     }
     return event;
 }
 
+// function to check if event is empty
+// returns 1 if empty, 0 if not
+int event_empty(event_t *e){
+    int isEmpty = -1;
+
+    if(e->queue == NULL){
+        isEmpty = 1;
+        // printf("Event Queue currently empty\n");
+    } else {
+        isEmpty = 0;
+    }
+    
+    return isEmpty;
+}
+
+// function to get the event's time
 double event_time(event_t *e) {
    return e->event_time;
+}
+
+// function to print current event queue
+void print_event(event_t *e) {
+    printf("Arrival Time: %f | AirlineQ: %f\n", e->passenger->arrival_time, e->passenger->airlineQ_time);
 }
